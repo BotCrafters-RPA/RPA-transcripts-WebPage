4c475b5cd206e4a18631c48dcc8b56c59a62ae1d
auza0002 <auzajuan1@gmail.com>
2024-03-14 12:51:31 -0400

    Header CSS completed small large sizes working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-
-
 const APP = {
   init() {
-    document.getElementById("mobile-menu-controller").addEventListener("click", APP.toogleHeaderMenu);
+    document
+      .getElementById("mobile-menu-controller")
+      .addEventListener("click", APP.toogleHeaderMenu);
+      document
+        .getElementById("background-color-controller")
+        .addEventListener("click", APP.toogleHeaderMenu);
   },
   toogleHeaderMenu() {
-    const nav = document.querySelector(".nav-list");
-    nav.classList.toggle("active")
+    const nav = document.querySelector(".nav-mobile");
+    nav.classList.toggle("active");
+    APP.toogleImageSrcMenu();
+  },
+  toogleImageSrcMenu() {
+    const imageElement = document.getElementById("mobile-menu-icon");
+    const barSolidAsset = "./assets/bars-solid.svg";
+    const xMarkSolidAssets = "./assets/xmark-solid.svg";
+    if (imageElement.src.includes("bars-solid")) {
+      imageElement.src = xMarkSolidAssets;
+    } else if (imageElement.src.includes("xmark-solid")) {
+      imageElement.src = barSolidAsset;
+    }
   },
 };
 document.addEventListener("DOMContentLoaded", APP.init);
